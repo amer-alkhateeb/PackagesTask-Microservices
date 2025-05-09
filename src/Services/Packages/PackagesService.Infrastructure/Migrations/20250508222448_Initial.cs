73b5fe5d9d0c9ecc967edd50acb4e3e5164e0fbd
Amer AlKhateeb <amer.m.alkhateeb@outlook.com>
2025-05-09 14:31:39 +0200

    FIxing namespaces bringing Apis to work dockerize the entire sevice

---
+++ src/Services/Packages/PackagesService.Infrastructure/Migrations/20250508222448_Initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PackagesService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "packages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Sender = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Recipient = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Destination_City = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Destination_Street = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Destination_ZIP = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Weight_Kilograms = table.Column<double>(type: "double precision", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_packages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "packages");
+        }
+    }
+}
