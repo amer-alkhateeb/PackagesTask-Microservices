248e6d097b9de783f12dfa5015181dc8ed70368c
Amer AlKhateeb <amer.m.alkhateeb@outlook.com>
2025-05-14 00:19:33 +0200

    Fixing DeliverySceduled Event Consumer

---
+++ src/Services/Packages/PackagesService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PackagesService.Application.Interfaces;
+using PackagesService.Application.Messaging;
+using PackagesService.Infrastructure.Persistence;
+namespace PackagesService.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("Postgres");
+
+            services.AddDbContext<PackageDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<DeliveryScheduledConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host("rabbitmq", "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+
+                    cfg.ReceiveEndpoint("package-service-queue", e =>
+                    {
+                        e.ConfigureConsumer<DeliveryScheduledConsumer>(context);
+                    });
+                });
+            });
+
+            services.AddScoped<IPackageRepository, PackagesRepository>();
+
+            return services;
+        }
+    }
+}
