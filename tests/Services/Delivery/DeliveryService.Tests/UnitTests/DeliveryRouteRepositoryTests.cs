70102eab88e148d4a408db9cf107333265fcc304
Amer AlKhateeb <amer.m.alkhateeb@outlook.com>
2025-05-24 02:24:58 +0200

    Delivery Unit Tests

---
+++ tests/Services/Delivery/DeliveryService.Tests/UnitTests/DeliveryRouteRepositoryTests.cs
@@ -0,0 +1,33 @@
+﻿using DeliveryService.Application.Interfaces;
+using DeliveryService.Domain.Models;
+using Moq;
+
+namespace DeliveryService.Tests.UnitTests
+{
+    public class DeliveryRouteRepositoryTests
+    {
+        private readonly Mock<IDeliveryRouteRepository> _repositoryMock;
+
+        public DeliveryRouteRepositoryTests()
+        {
+            _repositoryMock = new Mock<IDeliveryRouteRepository>();
+        }
+
+
+        [Fact]
+        public async Task GetByTrackingCodeAsync_WithNonExistingTrackingCode_ShouldReturnNull()
+        {
+            // Arrange
+            var trackingCode = "NONEXISTENT";
+
+            _repositoryMock.Setup(repo => repo.GetByTrackingCodeAsync(trackingCode, It.IsAny<CancellationToken>()))
+                .ReturnsAsync((Delivery)null);
+
+            // Act
+            var result = await _repositoryMock.Object.GetByTrackingCodeAsync(trackingCode, CancellationToken.None);
+
+            // Assert
+            Assert.Null(result);
+        }
+    }
+}
